04eebcd4ccce12a460fefac55bcdff85e8b34f95
rchauhan471 <rchauhan471@rku.ac.in>
2021-02-02 20:03:04 +0530

    fetching completed

---
+++ Model/book.js
@@ -0,0 +1,9 @@
+const mongoose = require('mongoose');
+
+const schema = mongoose.Schema({
+    name: String,
+    qty: Number
+
+})
+
+module.exports = mongoose.model("Book", schema);
---
+++ index.js
@@ -0,0 +1,16 @@
+const express = require('express');
+const mongoose = require('mongoose');
+const route = require('./route');
+
+
+// connect to mongodb 
+
+mongoose.connect("mongodb://localhost:27017/emp", { useNewUrlParser: true, useUnifiedTopology: true }).then(
+    () => {
+        const app = express();
+        app.use("/api", route);
+        app.listen(3000, () => {
+            console.log("server has started..!!");
+        })
+    }
+);
